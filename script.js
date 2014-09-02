428f832b0e6c6c9d1f97291f35c2363a2c9bfba3
Anvesh <anvesh.v@gmail.com>
2014-09-03 01:58:55 +0530

    with an error

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+	
+    numBox;
+	createGrid();
+
+});
+
+var numBox = prompt('number of boxes?','Enter a number from 1-40');
+
+
+
+
+
+
+var createGrid = function(numBox){
+	var gridSize = 960/numBox;
+
+	for (i=0;i<numBox;i++){
+		for(j=0;j<numBox;j++){
+			console.log(i);
+			$('.container').append('<div class="square"></div>');
+		}	
+
+	}
+	$('.square').css('height',gridSize);
+	$('.square').css('width',gridSize);
+};
